ed8ce5dd7d2be59aad1864aeccce2b19fca23000
ngoclan1102 <nguyenthiMgoclan@gmail.com>
2021-10-10 22:54:26 +0700

    add .idea

---
+++ script.js
@@ -0,0 +1,13 @@
+// function show() {
+//     document.write('xin chao!');
+// }
+// function show(){
+//   document.getElementByI("message").innerHTML = 'xin chao!'
+// }
+// function show(){
+//     alert('xin chao!');
+// }
+function show() {
+    document.getElementById("message").innerHTML = 'xin chao!';
+}
+
